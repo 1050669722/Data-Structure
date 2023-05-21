eb3bc32b18b47e966eacd2e01587e99903ddd074
1050669722 <1050669722@qq.com>
2023-05-21 12:21:01 +0800

    Add Graph; Replace pointers to smart pointers;

---
+++ Graph.h
@@ -0,0 +1,24 @@
+﻿//
+// Created by ASUS on 2023/5/21.
+//
+
+#ifndef DATA_STRUCTURE_GRAPH_H
+#define DATA_STRUCTURE_GRAPH_H
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+namespace graph
+{
+    // void addEdge(std::vector<std::vector<size_t>> *adjPtr, size_t u, size_t v);
+    void addEdge(std::shared_ptr<std::vector<std::vector<size_t>>> adjPtr, size_t u, size_t v);
+    void explore(const std::vector<std::vector<size_t>> &adj, size_t v,
+                 std::shared_ptr<std::vector<bool>> visitedPtr);
+    void depth_first_search(const std::vector<std::vector<size_t>> &adj,
+                            size_t start);
+
+}
+
+#endif //DATA_STRUCTURE_GRAPH_H
--- main.cpp
+++ main.cpp
@@ -4,9 +4,36 @@
 
 #include <iostream>
 
+#include "Graph.h"
+
 int main(int argc, char* argv[])
 {
     std::cout << "Hello World" << std::endl;
+
+    size_t vertices = 0, edges = 0;
+    std::cout << "Enter the Vertices : ";
+    std::cin >> vertices;
+    std::cout << "Enter the Edges : ";
+    std::cin >> edges;
+
+    /// creating graph
+    // std::vector<std::vector<size_t>> adj(vertices, std::vector<size_t>());
+    std::shared_ptr<std::vector<std::vector<size_t>>> adjPtr = std::make_shared<std::vector<std::vector<size_t>>>(vertices, std::vector<size_t>());
+
+    /// taking input for edges
+    std::cout << "Enter the vertices which have edges between them : "
+              << std::endl;
+    while (edges--) {
+        size_t u = 0, v = 0;
+        std::cin >> u >> v;
+        // graph::addEdge(&adj, u, v);
+        graph::addEdge(adjPtr, u, v);
+    }
+
+    /// running depth first search over graph
+    graph::depth_first_search(*adjPtr, 2);
+
+    std::cout << std::endl;
 
     return 0;
 }
